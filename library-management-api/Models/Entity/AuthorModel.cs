2a64e23affa9adf18d7f0801f889d98f7c6f920e
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-02-28 19:26:08 -0300

    feat: Adiciona entidade AuthorModel e seu mapeamento

---
+++ library-management-api/Models/Entity/AuthorModel.cs
@@ -0,0 +1,16 @@
+using System.Text.Json.Serialization;
+
+namespace library_management_api.Models.Entity;
+
+public class AuthorModel
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Name { get; set; }
+    public string Nationality { get; set; }
+
+    public Guid LibraryId { get; set; }
+    [JsonIgnore]
+    public LibraryModel Library { get; set; }
+    [JsonIgnore]
+    public IEnumerable<BookModel> Books { get; set; }
+}
