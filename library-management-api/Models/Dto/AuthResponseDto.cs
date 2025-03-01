396596072785574c69fa98322e7570cf2635e800
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-02-28 21:37:02 -0300

    feat: cria entidade de resposta para autenticação

---
+++ library-management-api/Models/Dto/AuthResponseDto.cs
@@ -0,0 +1,8 @@
+namespace library_management_api.Models;
+
+public class AuthResponseDto
+{
+    public string Name { get; set; }
+    public string Id { get; set; }
+    public string Token { get; set; }
+}
