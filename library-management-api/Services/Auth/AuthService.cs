927d3308eb9efd86181c2f414432d72f94f4bcbe
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-02-28 22:30:44 -0300

    feat: cria metodos Signup, GetAccessToken e VerifyAccessToken

--- library-management-api/Services/Auth/AuthService.cs
+++ library-management-api/Services/Auth/AuthService.cs
@@ -1,9 +1,32 @@
+using library_management_api.Data;
 using library_management_api.Models;
 using library_management_api.Models.Dto;
+using library_management_api.Models.Entity;
+using Microsoft.EntityFrameworkCore;
 
 namespace library_management_api.Services.Auth;
 
 public class AuthService : IAuthInterface
 {
-    
+    private readonly ApplicationDbContext _context;
+
+    public AuthService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public Task<ResponseModel<AuthResponseDto>> Signup(SignupRequestDto signupRequest)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GetAccessToken(string Id, string Name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<LibraryModel> VerifyAccessToken(string token)
+    {
+        throw new NotImplementedException();
+    }
 }
--- library-management-api/Services/Auth/IAuthInterface.cs
+++ library-management-api/Services/Auth/IAuthInterface.cs
@@ -1,9 +1,12 @@
 using library_management_api.Models;
 using library_management_api.Models.Dto;
+using library_management_api.Models.Entity;
 
 namespace library_management_api.Services.Auth;
 
 public interface IAuthInterface
 {
-    
+    Task<ResponseModel<AuthResponseDto>> Signup(SignupRequestDto signupRequest);
+    string GetAccessToken(string Id, string Name);
+    Task<LibraryModel> VerifyAccessToken(string token);
 }
