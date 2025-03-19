7b600a2cd718ee6e67bf0354e4f110c64cbb55c1
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-03-18 22:44:18 -0300

    feat: cria AddReaderRequestDto para dados de cadastro de leitores

---
+++ library-management-api/Models/Dto/AddReaderRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace library_management_api.Models.Dto;
+
+public class AddReaderRequestDto
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+}
