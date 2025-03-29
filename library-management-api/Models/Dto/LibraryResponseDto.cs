367c2c4dabf0d821c712904de72a9116f312661d
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-03-29 18:38:47 -0300

    feat: cria LibraryResponseDto para dados de resposta para os enpoints da Library

---
+++ library-management-api/Models/Dto/LibraryResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace library_management_api.Models.Dto;
+
+public class LibraryResponseDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public int TotalBooks { get; set; }
+    public int AvailableBooks { get; set; }
+    public int BorrowedBooks { get; set; }
+}
