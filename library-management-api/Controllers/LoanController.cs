3f4ad80e0ed9d3c8ab4278697d975a9662db3501
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-03-29 13:47:33 -0300

    feat: cria método e enpoint de RegisterNewLoan

---
+++ library-management-api/Controllers/LoanController.cs
@@ -0,0 +1,39 @@
+using library_management_api.Exceptions;
+using library_management_api.Models.Dto;
+using library_management_api.Models.Entity;
+using library_management_api.Services.Auth;
+using library_management_api.Services.Loan;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace library_management_api.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class LoanController : ControllerBase
+    {
+        private readonly ILoanInterface _loanInterface;
+        private readonly IAuthInterface _authInterface;
+
+        public LoanController(IAuthInterface authInterface, ILoanInterface loanService)
+        {
+            _authInterface = authInterface;
+            _loanInterface = loanService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ResponseModel<LoanModel>>> RegisterNewLoan(RegisterNewLoanRequestDto request)
+        {
+            var token = HttpContext.Request.Cookies["AuthCookie"];
+            var library = await _authInterface.VerifyAccessToken(token);
+            if (library is null)
+            {
+                throw new UnauthorizedException("Acesso negado!");
+            }
+            var response = await _loanInterface.RegisterNewLoan(library, request);
+            return Ok(response);
+        }
+    }
+}
--- library-management-api/Services/Loan/ILoanInterface.cs
+++ library-management-api/Services/Loan/ILoanInterface.cs
@@ -1,6 +1,9 @@
-﻿namespace library_management_api.Services.Loan;
+﻿using library_management_api.Models.Dto;
+using library_management_api.Models.Entity;
+
+namespace library_management_api.Services.Loan;
 
 public interface ILoanInterface
 {
-    
+    Task<ResponseModel<LoanModel>> RegisterNewLoan(LibraryModel library, RegisterNewLoanRequestDto request);
 }
