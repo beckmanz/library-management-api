01b74a0672dda40182511283f806cbecd7e9960b
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-03-29 22:56:38 -0300

    feat: cria validator para validação de dados de edição de bibliotecas

---
+++ library-management-api/Validators/EditLibraryRequestDtoValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using library_management_api.Models.Dto;
+
+namespace library_management_api.Validators;
+
+public class EditLibraryRequestDtoValidator : AbstractValidator<EditLibraryRequestDto>
+{
+    public EditLibraryRequestDtoValidator()
+    {
+        RuleFor(s => s.Name)
+            .MinimumLength(2).WithMessage("O nome deve ter pelo menos 2 caracteres!");
+
+        RuleFor(s => s.Email)
+            .EmailAddress().WithMessage("Email inválido!");
+        
+        RuleFor(s => s.NewPassword)
+            .MinimumLength(6).WithMessage("A senha deve ter pelo menos 6 caracteres!");
+    }
+}
