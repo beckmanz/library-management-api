aa4811d229853160923d99fd044d11ef44c889da
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-04-04 18:40:24 -0300

    feat: adiciona a propriedade "Token" na entidade de resposta de auth

--- library-management-api/Models/Dto/AuthResponseDto.cs
+++ library-management-api/Models/Dto/AuthResponseDto.cs
@@ -2,6 +2,7 @@ namespace library_management_api.Models;
 
 public class AuthResponseDto
 {
-    public string Name { get; set; }
     public string Id { get; set; }
+    public string Name { get; set; }
+    public string Token { get; set; }
 }
